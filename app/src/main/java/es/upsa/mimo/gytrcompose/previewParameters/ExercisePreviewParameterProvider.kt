203f1187b14a9b12af2754b1948fedb00a617101
Iván Fernández <ifernandezgo.inf@upsa.es>
2023-08-18 02:33:11 +0200

    Añadida clase para valores de preview de Exercises

--- app/src/main/java/es/upsa/mimo/gytrcompose/previewParameters/ExercisePreviewParameterProvider.kt
+++ app/src/main/java/es/upsa/mimo/gytrcompose/previewParameters/ExercisePreviewParameterProvider.kt
@@ -5,6 +5,6 @@ import es.upsa.mimo.gytrcompose.network.ExerciseDecoder
 
 class ExercisePreviewParameterProvider: PreviewParameterProvider<ExerciseDecoder> {
     override val values = sequenceOf(
-        ExerciseDecoder(id = "1", name = "Name", bodyPart = "Body part", equipment = "Equipment", gifUrl = "", target = "Target")
+        ExerciseDecoder(id = "1", name = "Name", bodyPart = "Body part", equipment = "Equipment", gifUrl = "https://api.exercisedb.io/image/vP4cByPx7gWQg3", target = "Target")
     )
 }
